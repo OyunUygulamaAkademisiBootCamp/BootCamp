4eb3bf51671a82ac1c2e019dd2bc9ef8fedb6ef2
Eray Aybek <aybek17@itu.edu.tr>
2022-05-07 16:32:28 +0300

    Added Sample Animation

--- Sisifos/Assets/Scripts/CharacterMovement.cs
+++ Sisifos/Assets/Scripts/CharacterMovement.cs
@@ -5,24 +5,28 @@ using UnityEngine.UIElements;
 
 public class CharacterMovement : MonoBehaviour
 {
-    
-    public float speed = 10;
+
+    public float speed;
     private int line = 1; // 0 = Left, 1 = Middle, 2 = Right
-    private float distance = 1.5f;
-    
+    private float distance = 2.5f;
+
     private Transform tf;
     private Vector3 charPos;
+
+
+
     private Vector3 targetcharPos;
     [SerializeField] private Transform plane;
-    
+
     //private float time = 0;
-    
+
+
     void Start()
     {
         tf = transform;
         if (plane != null)
         {
-            distance = (plane.localScale.z/3)* 10 ;
+            distance = (plane.localScale.z / 3) * 10;
         }
 
         Debug.Log(distance);
@@ -30,59 +34,75 @@ public class CharacterMovement : MonoBehaviour
 
     void Update()
     {
-        MoveForward(); // force ile yapabiliriz
+
+        Vector3 leftPos = new Vector3(charPos.x - distance, charPos.y, charPos.z);
+        Vector3 rightPos = new Vector3(charPos.x + distance, charPos.y, charPos.z);
         charPos = tf.position;
+
+
+        MoveForward();
+
         if (Input.GetKeyDown(KeyCode.LeftArrow) && line > 0)
         {
-            //Daha yumuşak bir geçiş için force uygulanabilir
-            Debug.Log(line);
-            targetcharPos = new Vector3(charPos.x - distance, charPos.y, charPos.z);
-            transform.position = Vector3.Lerp(charPos, targetcharPos, 1.0f);
-            line--;
-            
+            transform.position = Vector3.Lerp(transform.position, leftPos, 1f);
 
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow) && line < 2 )
-        {
-            Debug.Log(line);
-            targetcharPos = new Vector3(charPos.x + distance, charPos.y, charPos.z);
-            transform.position = targetcharPos; //Vector3.Lerp(charPos, targetcharPos, 1.0f);
-            line++;
-            
-        }
-        
-    }
-
-    void MoveForward()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-    }
-    
-
-    void lineMovement(bool rightMove)
-    {
-        // Left
-        if (!rightMove)
-        {
-            line--;
-            if (line == -1)
+            MoveForward(); // force ile yapabiliriz
+            charPos = tf.position;
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && line > 0)
             {
-                line = 0;
+                //Daha yumuşak bir geçiş için force uygulanabilir
+                Debug.Log(line);
+                targetcharPos = new Vector3(charPos.x - distance, charPos.y, charPos.z);
+                transform.position = Vector3.Lerp(charPos, targetcharPos, 1.0f);
+
+                line--;
+
             }
-            Debug.Log("Line: " + line);
-        }
-        // Right
-        else
-        {
-            line++;
-            if (line == 3)
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) && line < 2)
             {
-                line = 2;
+
+                transform.position = Vector3.Lerp(transform.position, rightPos, 1f);
+
+                Debug.Log(line);
+                targetcharPos = new Vector3(charPos.x + distance, charPos.y, charPos.z);
+                transform.position = targetcharPos; //Vector3.Lerp(charPos, targetcharPos, 1.0f);
+
+                line++;
+
             }
-            Debug.Log("Line: " + line);
+
         }
-        
+
+        void MoveForward()
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+
+        // void lineMovement(bool rightMove)
+        // {
+        //     // Left
+        //     if (!rightMove)
+        //     {
+        //         line--;
+        //         if (line == -1)
+        //         {
+        //             line = 0;
+        //         }
+        //         Debug.Log("Line: " + line);
+        //     }
+        //     // Right
+        //     else
+        //     {
+        //         line++;
+        //         if (line == 3)
+        //         {
+        //             line = 2;
+        //         }
+        //         Debug.Log("Line: " + line);
+        //     }
+
         // Vector3 targetPos = transform.position.x * Vector3.forward;
         //
         //
@@ -95,5 +115,6 @@ public class CharacterMovement : MonoBehaviour
         //     targetPos += Vector3.right * distance;
         // }
         //
+        //}
     }
 }
